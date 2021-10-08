09cd78831dadc1db68b5d7e3591f6a885a592f65
migueldeoleiros <migueldeoleiros@gmail.com>
2021-10-08 09:46:14 +0200

    ejercicio 2 completo

--- p2/fact.ml
+++ p2/fact.ml
@@ -1,4 +1,7 @@
-let rec fact n =
-    if n = 0 then 1
-    else n * fact (n-1);;
-
+let rec fact = function
+  0 -> 1
+  | n -> n * fact (n- 1)
+let size = Array.length Sys.argv
+let _ =
+  if size <> 2 then print_endline "error"
+  else print_endline(string_of_int(fact(int_of_string(Array.get Sys.argv(1)))))
