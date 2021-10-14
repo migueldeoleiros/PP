fbd28839dafdc230b52c930eaf16e8f87cfaf91f
migueldeoleiros <migueldeoleiros@gmail.com>
2021-10-14 13:14:31 +0200

    añadido comentario pertinente

--- p4/power.ml
+++ p4/power.ml
@@ -7,6 +7,11 @@ let rec power' x n =
     else if (n mod 2 = 0) then power' (x*x) (n/2)
     else x * power' (x*x) (n/2)
 
+(*
+La función power' realiza menos operaciones dado que reduce n a la mitad recursivamente.
+Esto será notable en caso de números grandes, que con int se salen de rango
+ *)
+
 let rec powerf x n = 
     if n = 0 then 1. 
     else if (n mod 2 = 0) then powerf' (x*.x) (n/2)
