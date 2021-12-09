32545907d7add67d2ea23397ae94a882ea597f24
migueldeoleiros <migueldeoleiros@gmail.com>
2021-12-09 14:00:48 +0100

    pequeno avance en g_tree

--- p12/g_tree.ml
+++ p12/g_tree.ml
@@ -1,4 +1,9 @@
-type 'a g_tree = Gt of 'a * 'a g_tree list;;
+type 'a g_tree =
+    Gt of 'a * 'a g_tree list;;
+
+(* type 'a bin_tree =
+ *     Empty
+ *   | Node of 'a * 'a bin_tree * 'a bin_tree;; *)
 
 (* val size : 'a g_tree -> int *)
 (* devuelve el número de nodos de un g_tree *)
@@ -8,40 +13,49 @@ let rec size = function
 
 let rec size = function
 	Empty -> 0
-	| Node (r,i,d) -> (size i) + (size d) + 1;;
+  | Node (r,i,d) -> (size i) + (size d) + 1;;
 
 (* val height : 'a g_tree -> int *)
 (* devuelve la "altura", como número de niveles, de un g_tree *)
 
+let rec height = function 
+    Gt (_,[]) -> 1
+  | Gt (r,h::t) -> (max (height h) (height (Gt (r,t))));;
+
+let rec height = function
+	Empty -> 0
+  | Node (r,i,d) -> (max (height i) (height d)) +1;;
+	
 (* val leaves : 'a g_tree -> 'a list *)
 (* devuelve las hojas de un g_tree, "de izquierda a derecha" *)
 
+let rec leaves = function 
+    Gt (_,[]) -> []
+  | Gt (r,[]::[]) -> [r]
+  | Gt (r,h::t) -> leaves h + leaves (Gt (r,t));;
+
+let rec leaves = function
+	Empty -> []
+  | Node (r,Empty,Empty) -> [r]
+  | Node (r,i,d) -> leaves i @ leaves d;;
+	
 (* val mirror : 'a g_tree -> 'a g_tree *)
 (* devuelve la imagen especular de un g_tree *)
+
+let rec mirror = function
+	Empty -> Empty
+  | Node (r,i,d) -> Node (r,d,i);;
 
 (* val preorder : 'a g_tree -> 'a list *)
 (* devuelve la lista de nodos de un g_tree en "preorden" *)
 
+let rec preorder = function
+	Empty -> []
+  | Node (r,i,d) -> r::(preorder i) @ (preorder d);;
+
 (* val postorder : 'a g_tree -> 'a list *)
 (* devuelve la lista de nodos de un g_tree en "postorden" *)
 
-
-let rec sum = function
-    Empty -> 0
-  | Node (r, lb, rb) -> r + (sum lb) + (sum rb);;
-
-let rec prod = function
-    Empty -> 1.0
-  | Node (r, lb, rb) -> r *. (prod lb) *. (prod rb);;
-
-let rec size = function
-	Empty -> 0
-	| Node (r,i,d) -> (size i) + (size d) + 1;;
-
-let rec inorder = function
-    Empty -> []
-  | Node (r, lb, rb) -> inorder lb @ (r :: inorder rb);;
-
-let rec mirror = function
-	Empty -> Empty
-	| Node (r,i,d) -> Node (r,d,i);;
+let rec postorder = function
+	Empty -> []
+  | Node (r,i,d) -> (postorder i) @ (postorder d) @ [r];;
