44d9a61b53c3fe829119cb166de8ec957ba4f730
migueldeoleiros <migueldeoleiros@gmail.com>
2022-01-11 13:42:42 +0100

    terminado breath_first terminal

--- p12/breadth_first.ml
+++ p12/breadth_first.ml
@@ -6,6 +6,8 @@ let rec breadth_first = function
   | Gt (x, (Gt (y, t2))::t1) -> x :: breadth_first (Gt (y, t1@t2));;
 
 (* version terminal de breadth_first *)
-let rec breadth_first_t = function
-    Gt (x, []) -> [x]
-  | Gt (x, (Gt (y, t2))::t1) -> x :: breadth_first (Gt (y, t1@t2));;
+let rec breadth_first_t t =
+  let rec aux acum = function
+      Gt (x, []) -> List.rev (x::acum)
+    | Gt (x, (Gt (y, t2))::t1) -> aux (x::acum) (Gt (y, List.rev_append (List.rev t1) t2))
+  in aux [] t;;
